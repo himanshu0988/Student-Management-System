ce8f20cda9859d5067d25d0eef8da942edf63a6f
Himanshu Kumar Singh <127866082+himanshu0988@users.noreply.github.com>
2024-05-28 11:03:56 +0530

    Update JdbcUtil.java

--- src/in/student/connection/JdbcUtil.java
+++ src/in/student/connection/JdbcUtil.java
@@ -10,7 +10,6 @@ import java.sql.Statement;
 import java.util.Properties;
 
 public class JdbcUtil {
-	
 	// restrict the object creation from outside of class
 	private JdbcUtil(){}
 	static {
@@ -20,7 +19,6 @@ public class JdbcUtil {
 				e.printStackTrace();
 			}
 		}
-		
 		public static Connection getConnection() throws SQLException, IOException {
 			
 			FileInputStream fis=new FileInputStream("C:\\Users\\HIMANSHU\\eclipse-workspace\\StudentManagementSystem\\src\\in\\student\\connection\\application.properties");
@@ -29,16 +27,5 @@ public class JdbcUtil {
 			
 			Connection connection=DriverManager.getConnection(properties.getProperty("url"),properties.getProperty("user"),properties.getProperty("password"));
 			return connection;
-		}
-		
-		public static void clenUp(Connection connection,Statement statement, ResultSet resultset) throws SQLException {
-			if(connection!=null)
-				connection.close();
-	
-			if(statement!=null)
-				statement.close();
-			
-			if(resultset!=null)
-				resultset.close();
 		}
 }
